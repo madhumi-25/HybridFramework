d276b93eccadf0840f0b0673c668e16c7b741515
madhumi-25 <60865917+madhumi-25@users.noreply.github.com>
2020-03-27 11:44:54 +0530

    Update PracticeAutomationRunnerClass.java
    Full Project

--- ProjectPracticeAutomation/src/test/java/com/practiceautomation/runner/PracticeAutomationRunnerClass.java
+++ ProjectPracticeAutomation/src/test/java/com/practiceautomation/runner/PracticeAutomationRunnerClass.java
@@ -21,7 +21,13 @@ import cucumber.api.junit.Cucumber;
 		@AfterClass
 	    public static void writeExtentReport() 
 		{
-	        	Reporter.loadXMLConfig("src/test/resources/extent-config.xml");  
+	        	Reporter.loadXMLConfig("src/test/resources/extent-config.xml"); 
+		    	Reporter.setSystemInfo("User Name", System.getProperty("user.name"));
+	      		Reporter.setSystemInfo("Time Zone", System.getProperty("user.timezone"));
+	     		Reporter.setSystemInfo("Machine", "Windows 10" + "64 Bit");
+	      		Reporter.setSystemInfo("Selenium", "3.141.59");
+	      		Reporter.setSystemInfo("Maven", "4.0.0");
+	      		Reporter.setSystemInfo("Java Version", "1.8.0_131");
 	    	}
 	}                                      
 
